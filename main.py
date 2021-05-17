2752d48d6bd32e10d5fe20f68e3b5bac388ef20a
cmcool <75821231+cmcool@users.noreply.github.com>
2021-05-17 17:54:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -50,6 +50,123 @@ async def meme(ctx):
 		await ctx.send(embed=embed)
 #------------------------------Other Games-------------------------------------------   
 
+# tic tac toe
+player1 = ""
+player2 = ""
+turn = ""
+gameOver = True
+
+board = []
+
+winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+@client.command()
+async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
+    global count
+    global player1
+    global player2
+    global turn
+    global gameOver
+
+    if gameOver:
+        global board
+        board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:"]
+        turn = ""
+        gameOver = False
+        count = 0
+
+        player1 = p1
+        player2 = p2
+
+        # print the board
+        line = ""
+        for x in range(len(board)):
+            if x == 2 or x == 5 or x == 8:
+                line += " " + board[x]
+                await ctx.send(line)
+                line = ""
+            else:
+                line += " " + board[x]
+
+        # determine who goes first
+        num = random.randint(1, 2)
+        if num == 1:
+            turn = player1
+            await ctx.send("It is <@" + str(player1.id) + ">'s turn.")
+        elif num == 2:
+            turn = player2
+            await ctx.send("It is <@" + str(player2.id) + ">'s turn.")
+    else:
+        await ctx.send("A game is already in progress! Finish it before starting a new one.")
+
+@client.command()
+async def place(ctx, pos: int):
+    global turn
+    global player1
+    global player2
+    global board
+    global count
+    global gameOver
+
+    if not gameOver:
+        mark = ""
+        if turn == ctx.author:
+            if turn == player1:
+                mark = ":regional_indicator_x:"
+            elif turn == player2:
+                mark = ":o2:"
+            if 0 < pos < 10 and board[pos - 1] == ":white_large_square:" :
+                board[pos - 1] = mark
+                count += 1
+
+                # print the board
+                line = ""
+                for x in range(len(board)):
+                    if x == 2 or x == 5 or x == 8:
+                        line += " " + board[x]
+                        await ctx.send(line)
+                        line = ""
+                    else:
+                        line += " " + board[x]
+
+                checkWinner(winningConditions, mark)
+                print(count)
+                if gameOver == True:
+                    await ctx.send(mark + " wins!")
+                elif count >= 9:
+                    gameOver = True
+                    await ctx.send("It's a tie!")
+
+                # switch turns
+                if turn == player1:
+                    turn = player2
+                elif turn == player2:
+                    turn = player1
+            else:
+                await ctx.send("Be sure to choose an integer between 1 and 9 (inclusive) and an unmarked tile.")
+        else:
+            await ctx.send("It is not your turn.")
+    else:
+        await ctx.send("Please start a new game using the .tictactoe command.")
+
+
+def checkWinner(winningConditions, mark):
+    global gameOver
+    for condition in winningConditions:
+        if board[condition[0]] == mark and board[condition[1]] == mark and board[condition[2]] == mark:
+            gameOver = True
+
 # rock paper scissors
 @client.command(help="Play with .rps [your choice]")
 async def rps(ctx):
@@ -74,9 +191,9 @@ async def rps(ctx):
         if comp_choice == 'rock':
             await ctx.send(f'HAHA {ctx.author.mention}, you are so bad you lost!!!\nYour choice: {user_choice}\nMy choice: {comp_choice}')
         elif comp_choice == 'paper':
-            await ctx.send(f'Oh, what?{ctx.author.mention} we just tied. I call a rematch!!\nYour choice: {user_choice}\nMy choice: {comp_choice}')
+            await ctx.send(f'Oh, what? {ctx.author.mention} we just tied. I call a rematch!!\nYour choice: {user_choice}\nMy choice: {comp_choice}')
         elif comp_choice == 'scissors':
-            await ctx.send(f"{ctx.author.mention}You only won because you are cheating smh.\nYour choice: {user_choice}\nMy choice: {comp_choice}")
+            await ctx.send(f"{ctx.author.mention} you only won because you are cheating smh.\nYour choice: {user_choice}\nMy choice: {comp_choice}")
 
     elif user_choice == 'scissors':
         if comp_choice == 'rock':
@@ -733,7 +850,7 @@ async def help(ctx):
     with open("commands.txt", "r") as f:
         scores = f.read().splitlines()
 
-    final = '\n'.join(scores[0:49])
+    final = '\n'.join(scores[0:53])
     embed=discord.Embed(title="Commands", description=final)
     embed.colour = (0x964b00)
     await ctx.send(embed = embed)
@@ -767,19 +884,20 @@ async def invite(ctx):
 #sends the help server invite when .srvinvite is input
 @client.command()
 async def srvinvite(ctx):
-  await ctx.send('discord.gg/7rMg2txgyy')
+  await ctx.send('discord.gg/HpEGNvur28')
 
 # sends the bot's top.gg page when .upvote is input
 @client.command()
 async def upvote(ctx):
-  await ctx.send('You can upvote the bot here: https://top.gg/bot/778424945607704576/vote')
+  await ctx.send('You can upvote me here: https://top.gg/bot/778424945607704576/vote')
 
 # sends the bot's website link
 @client.command()
 async def website(ctx):
   await ctx.send('https://sites.google.com/view/monke-bot')
 
-# error handeling
+# -----------------------------------error handeling----------------------------------
+# economy
 @client.event
 async def on_command_error(ctx,error):
   if isinstance(error,commands.CommandOnCooldown):
@@ -790,6 +908,22 @@ async def on_command_error(ctx,error):
     embed=discord.Embed(title="WAIT!  ⏰", description=final)
     embed.colour = (0xFF0000)
     await ctx.send(embed = embed)
+
+# tic tac toe
+@tictactoe.error
+async def tictactoe_error(ctx, error):
+    print(error)
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please mention 2 players for this command.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Please make sure to mention/ping players (ie. <@688534433879556134>).")
+
+@place.error
+async def place_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please enter a position you would like to mark.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Please make sure to enter an integer.")
 
 # connects bot to discord
 keep_alive.keep_alive()
